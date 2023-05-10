e9aa6bf7b8a06e864a232ca6057131c7cb235c5f
NethminiDevyanjalee <nethminid1999@gmail.com>
2023-05-10 17:24:06 +0530

    Implement the application page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./view/pages/Home";
+import Application from "./view/pages/Application";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/application" element={<Application />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/view/pages/Application.js
@@ -0,0 +1,44 @@
+import resets from '../resets.module.css';
+import classes from './Application.module.css';
+import submitIcon from '../../images/submitIcon.png';
+import applicationImage from '../../images/applicationImage.svg';
+
+export default function Application() {
+    return(
+        <div className={`${resets.storybrainResets} ${classes.root}`}>
+            <div className={classes.application}>Application</div>
+            <div className={classes.copyright2023}>
+                <p className={classes.labelWrapper}>
+                    <span className={classes.label}>C</span>
+                    <span className={classes.label2}>opyright @2023</span>
+                </p>
+            </div>
+            <div className={classes.applyTodayForTheGramaCertifica}>Apply today for the Grama Certificate</div>
+            <button className={classes.saveButton}>
+                <div className={classes.saveButton2}></div>
+                <div className={classes.saveButton3}></div>
+                <div className={classes.frame46}>
+                    <div className={classes.submit}>Submit</div>
+                    <div className={classes.envelope}>
+                        <img src={submitIcon} alt="SubmitIcon" className={classes.icon4} />
+                    </div>
+                </div>
+            </button>
+            <div className={classes.undraw_personal_file_re_5joy1}>
+                <img src={applicationImage} alt="ApplicationImage" className={classes.icon5} />
+            </div>
+            <div className={classes.frame51}>
+                <div className={classes.frame49}>
+                    <div className={classes.iD}>ID</div>
+                    <div className={classes.firstNameBox}></div>
+                </div>
+                <div className={classes.frame50}>
+                    <div className={classes.address}>Address</div>
+                    <div className={classes.firstNameBox2}></div>
+                </div>
+            </div>
+            <div className={classes.editProfile}></div>
+        </div>
+    );
+
+}
