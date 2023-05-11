ce267ab4fbe95fec1f4fef156b97ba2440ef0370
NethminiDevyanjalee <nethminid1999@gmail.com>
2023-05-11 18:28:24 +0530

    Update CSS file

--- src/view/pages/Application.js
+++ src/view/pages/Application.js
@@ -6,38 +6,31 @@ import applicationImage from '../../assets/images/applicationImage.svg';
 export default function Application() {
     return(
         <div className={`${resets.storybrainResets} ${classes.root}`}>
-            <div className={classes.application}>Application</div>
-            <div className={classes.copyright2023}>
-                <p className={classes.labelWrapper}>
-                    <span className={classes.label}>C</span>
-                    <span className={classes.label2}>opyright @2023</span>
-                </p>
-            </div>
-            <div className={classes.applyTodayForTheGramaCertifica}>Apply today for the Grama Certificate</div>
-            <button className={classes.saveButton}>
-                <div className={classes.saveButton2}></div>
-                <div className={classes.saveButton3}></div>
-                <div className={classes.frame46}>
-                    <div className={classes.submit}>Submit</div>
-                    <div className={classes.envelope}>
-                        <img src={submitIcon} alt="SubmitIcon" className={classes.icon4} />
+            <div className={classes.applicationHeader}>Application</div>
+            <div className={classes.applicationDescription}>Apply today for the Grama Certificate</div>
+            <button className={classes.applicationSubmitButton}>
+                <div className={classes.submitButton}></div>
+                <div className={classes.submitFrame}>
+                    <div className={classes.submitDescription}>Submit</div>
+                    <div className={classes.submitIcon}>
+                        <img src={submitIcon} alt="SubmitIcon" className={classes.applicationIcon} />
                     </div>
                 </div>
             </button>
-            <div className={classes.undraw_personal_file_re_5joy1}>
-                <img src={applicationImage} alt="ApplicationImage" className={classes.icon5} />
+            <div className={classes.applicationImage}>
+                <img src={applicationImage} alt="ApplicationImage" className={classes.applicationIcon} />
             </div>
-            <div className={classes.frame51}>
-                <div className={classes.frame49}>
-                    <div className={classes.iD}>ID</div>
-                    <div className={classes.firstNameBox}></div>
+            <div className={classes.formFrame}>
+                <div className={classes.formField}>
+                    <div className={classes.formLabel}>ID</div>
+                    <div className={classes.formInput}></div>
                 </div>
-                <div className={classes.frame50}>
-                    <div className={classes.address}>Address</div>
-                    <div className={classes.firstNameBox2}></div>
+                <div className={classes.formField}>
+                    <div className={classes.formLabel}>Address</div>
+                    <div className={classes.formInput}></div>
                 </div>
             </div>
-            <div className={classes.editProfile}></div>
+            <div className={classes.applicationFormFrame }></div>
         </div>
     );
 
