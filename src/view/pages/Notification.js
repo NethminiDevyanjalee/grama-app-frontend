25d6d510edd6a58df3cc9f3867e970ffec65a7df
Hasini <hasinisama99@gmail.com>
2023-05-12 10:02:17 +0530

    Basic page layout

--- src/view/pages/Notification.js
+++ src/view/pages/Notification.js
@@ -1,5 +1,23 @@
+import customerSupport from '../../assets/images/customer_support.png';
+
 function Notification() {
-    return <h1>Welcome to the notification page!</h1>;
+    return (
+        <div className="notificationPage">
+            <div className="leftColumn">
+                <h1>Need some Help?</h1>
+                <h2>Just write us a message!</h2>
+                <p>Fill up the form and our team will get back to you within 24 hours.</p>
+                <textarea placeholder="Write your message..."></textarea>
+                <button>
+                    <i className="icon"></i>
+                    Send
+                </button>
+            </div>
+            <div className="rightColumn">
+                <img src={customerSupport} alt="Customer Support" />
+            </div>
+        </div>
+    );
 }
 
 export default Notification;
