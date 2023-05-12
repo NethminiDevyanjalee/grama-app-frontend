3e32f80f2e90b19b5c7e8e82ed8794253d2e2a9c
NethminiDevyanjalee <nethminid1999@gmail.com>
2023-05-12 16:30:31 +0530

    Implement form functionality

--- src/view/pages/Application.js
+++ src/view/pages/Application.js
@@ -1,36 +1,68 @@
+import React, { useState } from 'react';
 import resets from '../resets.module.css';
 import classes from './Application.module.css';
 import submitIcon from '../../assets/images/submitIcon.png';
 import applicationImage from '../../assets/images/applicationImage.svg';
 
 export default function Application() {
+    const [id, setId] = useState('');
+    const [address, setAddress] = useState('');
+    const [error, setError] = useState('');
+
+    const handleIdChange = (event) => {
+        setId(event.target.value);
+    };
+
+    const handleAddressChange = (event) => {
+        setAddress(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (!id) {
+            setError('⚠ Please enter your ID');
+            return;
+          }
+        
+          if (!address) {
+            setError('⚠ Please enter your address');
+            return;
+          }
+
+        window.location.href = '/status';
+    };
+
     return(
         <div className={`${resets.storybrainResets} ${classes.root}`}>
             <div className={classes.applicationHeader}>Application</div>
             <div className={classes.applicationDescription}>Apply today for the Grama Certificate</div>
-            <button className={classes.applicationSubmitButton}>
-                <div className={classes.submitButton}></div>
-                <div className={classes.submitFrame}>
-                    <div className={classes.submitDescription}>Submit</div>
-                    <div className={classes.submitIcon}>
-                        <img src={submitIcon} alt="SubmitIcon" className={classes.applicationIcon} />
+            <form onSubmit={handleSubmit}>
+                <div className={classes.applicationFormFrame }></div>
+                <div className={classes.formFrame}>
+                    <div className={classes.formError}>{error}</div>
+                    <div className={classes.formField}>
+                        <div className={classes.formLabel}>ID</div>
+                        <input type="text" className={classes.formInput} value={id} placeholder='Enter ID' onChange={handleIdChange} />                    
+                    </div>
+                    <div className={classes.formField}>
+                        <div className={classes.formLabel}>Address</div>
+                        <input type="text" className={classes.formInput}  value={address} placeholder='Enter Address' onChange={handleAddressChange} />
                     </div>
                 </div>
-            </button>
+                <button type="submit" className={classes.applicationSubmitButton}>
+                    <div className={classes.submitButton}></div>
+                    <div className={classes.submitFrame}>
+                        <div className={classes.submitDescription}>Submit</div>
+                        <div className={classes.submitIcon}>
+                            <img src={submitIcon} alt="SubmitIcon" className={classes.applicationIcon} />
+                        </div>
+                    </div>
+                </button>
+            </form>
             <div className={classes.applicationImage}>
                 <img src={applicationImage} alt="ApplicationImage" className={classes.applicationIcon} />
-            </div>
-            <div className={classes.formFrame}>
-                <div className={classes.formField}>
-                    <div className={classes.formLabel}>ID</div>
-                    <div className={classes.formInput}></div>
-                </div>
-                <div className={classes.formField}>
-                    <div className={classes.formLabel}>Address</div>
-                    <div className={classes.formInput}></div>
-                </div>
-            </div>
-            <div className={classes.applicationFormFrame }></div>
+            </div>  
         </div>
     );
 
