1f332d910e7bc1d1928824d4055f2f4fb7cd106b
Hasini Samarathunga <hasinis@wso2.com>
2023-05-16 16:48:30 +0530

    Added landing page UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './view/components/Navbar';
 import Notification from './view/pages/Notification';
 import Help from './view/pages/Help';
+import Landing from './view/pages/Landing';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/apply" element={<Apply />} />
           <Route path="/status" element={<Status />} />
           <Route path="/notifications" element={<Notification />} />
