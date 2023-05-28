82cb3d0d1e906a90272631eebd7116dbc28f1022
NethminiDevyanjalee <nethminid1999@gmail.com>
2023-05-28 16:54:56 +0530

    Updated tab alignments

--- src/view/components/Navbar.js
+++ src/view/components/Navbar.js
@@ -24,10 +24,10 @@ function Navbar() {
             <img src={logo} alt="Logo" className='logo'/>
             <Box className="nav-item" sx={{ width: '100%' }}>
               <Tabs indicatorColor={"white"} textColor={"white"} value={value} onChange={handleChange} aria-label="nav tabs example">
-                <Tab label="Home" component={NavLink} to="/home" />
+                <Tab label="Home" component={NavLink} to="/home" sx={{marginLeft: '350px'}}/>
                 <Tab label="Apply" component={NavLink} to="/apply" />
                 <Tab label="Status" component={NavLink} to="/status" />
-                <Tab label="Help" component={NavLink} to="/help" />
+                <Tab label="Help" component={NavLink} to="/help" sx={{marginRight: '350px'}} />
                 <Tab className="nav-icon gap" component={NavLink} to="/notifications" icon={<img src={notificationIcon} alt="Notification" />} />
                 <Tab className="nav-icon" component={NavLink} to="/profile" icon={<img src={profileIcon} alt="Profile" />} />
               </Tabs>
